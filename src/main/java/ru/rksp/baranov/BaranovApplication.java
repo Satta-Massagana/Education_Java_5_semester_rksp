882b53e0ae7ad0d1d07a33dc2d6fa4be72984ea4
Satta-Massagana <satta444@gmail.com>
2025-10-19 16:02:14 +0500

    Lab-1

---
+++ src/main/java/ru/rksp/baranov/BaranovApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.baranov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaranovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaranovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/baranov/BaranovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.baranov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaranovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
